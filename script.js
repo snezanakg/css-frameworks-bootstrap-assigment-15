e65fa8d2fcf22b5c0ad36373875aa2d65232101a
snezanakg <142974537+snezanakg@users.noreply.github.com>
2024-01-04 11:39:42 +0100

    script,js,style/index.css

---
+++ script.js
@@ -0,0 +1,8 @@
+//change bbackground color on button click
+const btn = document.getElementByld('MyButton');
+const body=document.body;
+
+GamepadButton.AddEventListener('click',
+function(){
+    body.style.backgroundcolor='blue';
+})
